ea22c8c8a43ccb9eea68701460f82b81e95f5fd3
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-12-12 17:46:44 -0500

    Revert "update deployed site"
    This reverts commit b9e4db2980d2776a221fa923f16ccc8eca1f1d53.

---
+++ docs/viewer/precache-manifest.c36c307e23716c9992f98ff5ec32755f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bce75be7dbfd44b7c2e262a2b23faf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e77cea04c69a1d971d2c",
+    "url": "/static/css/main.16905b4a.chunk.css"
+  },
+  {
+    "revision": "fee509326ef3f87a0ce8",
+    "url": "/static/js/2.11b8ea54.chunk.js"
+  },
+  {
+    "revision": "e77cea04c69a1d971d2c",
+    "url": "/static/js/main.02a7cf65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
